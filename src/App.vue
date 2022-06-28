8d5619c5c342aee2dae4916f83681dc5c0c07ecf
dupeng <dupeng@mxnavi.com>
2022-06-28 13:36:57 +0800

    '上传代码'

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { whenever  } from '@vueuse/core'
+import {ref} from 'vue'
+
+const source = ref(false)
+
+whenever(
+  source,
+  (value) => {
+    console.log('当前值',value)
+  },
+)
+
+const clickedFn = () => {
+  source.value = !source.value
+}
+
+</script>
+
+<template>
+ <div>{{source}}</div>
+  <button @click="clickedFn">
+    点击按钮
+  </button>
+</template>
